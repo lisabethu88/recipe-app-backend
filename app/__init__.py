9636a33ac9697623c284c349e433e46a0e84b30d
Lisa Utsett <lisabethutsett@gmail.com>
2024-05-22 19:35:14 -0400

    Add blueprints to init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from .routes.user_routes import users_bp
+from .routes.recipe_routes import recipes_bp
+from .routes.category_routes import categories_bp
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.register_blueprint(recipes_bp)
+    app.register_blueprint(users_bp)
+    app.register_blueprint(categories_bp)
 
     return app
